e3c2b82aab3a2a7444525c6b7eea4ce5628cc37d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-11-11 22:56:47 -0300

    feat: Routes config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import registerRoutes from './register.routes';
+import loginRoutes from './login.routes';
 
 const router = Router();
 
-router.get('/', (_request: Request, response: Response) => {
-  return response.json({ message: 'API is running' });
+router.get('/', (_req: Request, res: Response) => {
+  return res.status(200).json({ message: 'API is running' });
 });
+
+router.use('/auth/register', registerRoutes);
+router.use('/auth/login', loginRoutes);
 
 export default router;
